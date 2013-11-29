0d1a798fb0bda29f02a59508b1c3fabc54053fe0
Andy Awidarto <andy.awidarto@gmail.com>
2013-11-29 15:40:09 +0700

    prop grid

--- app/views/contact/landing.blade.php
+++ app/views/contact/landing.blade.php
@@ -1,7 +1,31 @@
-@extends('layout.front')
+@extends('realia.layout')
 
 @section('content')
-    <h2>Thank You for Contacting Us</h2>
-    <p>We will contact you back for further discussions.</p>
-    <p>Have a Good Day !</p>
+
+<div class="container">
+    <div id="main">
+
+        <div class="row">
+            <div class="span9">
+
+                <h1 class="page-header">Thank You for Contacting Us</h1>
+                <p>We will contact you back for further discussions.</p>
+                <p>Have a Good Day !</p>
+
+                <!--insert grid-->
+            </div>
+            <div class="sidebar span3">
+
+                @include('realia.latest')
+
+
+            </div>
+        </div>
+
+    <!--insert carousel-->
+    <!--insert features-->
+    </div>
+</div>
+
+
 @stop
