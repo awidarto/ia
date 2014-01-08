2051b1b16e442c1aca9467fa09344e0e07872dd6
Andy Awidarto <andy.awidarto@gmail.com>
2014-01-08 10:55:40 +0700

    transaction

--- public/themes/default/views/login.blade.php
+++ public/themes/default/views/login.blade.php
@@ -3,71 +3,74 @@
 @section('content')
             <!-- if there are login errors, show them here -->
 <div class="row" style="margin-left:8px;padding-bottom:0px;margin-top:10px;">
-    <div class="span3">
-        <h4>Agent Login</h4>
-            {{Former::open_horizontal('login','POST',array('class'=>''))}}
-                @if (Session::get('loginError'))
-                    <div class="alert alert-danger">{{ Session::get('loginError') }}</div>
-                         <button type="button" class="close" data-dismiss="alert"></button>
-                @endif
-                {{ Former::text('email','Email')->class('span2') }}
+    <div class="span8">
+        <div class="row">
+            <div class="span3">
+                <h4>Agent Login</h4>
+                    {{Former::open_horizontal('login','POST',array('class'=>''))}}
+                        @if (Session::get('loginError'))
+                            <div class="alert alert-danger">{{ Session::get('loginError') }}</div>
+                                 <button type="button" class="close" data-dismiss="alert"></button>
+                        @endif
+                        {{ Former::text('email','Email')->class('span2') }}
 
-                {{ Former::password('password','Password')->class('span2') }}
+                        {{ Former::password('password','Password')->class('span2') }}
 
-                {{-- Former::checkbox('remember-me')->label('')->text('Remember Me')->value('remember-me') --}}
+                        {{-- Former::checkbox('remember-me')->label('')->text('Remember Me')->value('remember-me') --}}
 
-                {{ Former::submit('Login')->class('btn btn-primary pull-right') }}
+                        {{ Former::submit('Login')->class('btn btn-primary pull-right') }}
 
-            {{ Former::close() }}
+                    {{ Former::close() }}
+            </div>
+            <div class="span1"></div>
+            <div class="span3">
+                <h4>Customer Login</h4>
+                    {{Former::open_horizontal('clogin','POST',array('class'=>''))}}
+                        @if (Session::get('cloginError'))
+                            <div class="alert alert-danger">{{ Session::get('cloginError') }}</div>
+                                 <button type="button" class="close" data-dismiss="alert"></button>
+                        @endif
+                        {{ Former::text('email','Email')->class('span2') }}
+
+                        {{ Former::password('password','Password')->class('span2') }}
+
+                        {{-- Former::checkbox('remember-me')->label('')->text('Remember Me')->value('remember-me') --}}
+
+                        {{ Former::submit('Login')->class('btn btn-primary pull-right') }}
+
+                        <div class="" style="display:block;padding-top:20px;text-align:right;clear:both;width:100%;">
+                            <a href="{{ URL::to('account/create') }}" class="" >Create account</a> here
+                        </div>
+
+                    {{ Former::close() }}
+            </div>
+        </div>
+        <div class="row">
+            <div class="liquid-slider span7" id="home-slider">
+
+                @foreach($featured as $f)
+                 <div>
+                      <h4 class="title">{{ $f['number'].' '.$f['address'].','.$f['city'].' '.$f['state'] }}</h4>
+                      <div class="row">
+                        <div class="span2" >
+                            <img src="{{ $f['defaultpictures']['thumbnail_url']}}" alt="{{ $f['propertyId'] }}" />
+                        </div>
+                        <div class="span3" >
+                            {{ $f['description']}}
+                        </div>
+                      </div>
+                 </div>
+                @endforeach
+            </div>
+
+        </div>
     </div>
-    <div class="span1"></div>
-    <div class="span3">
-        <h4>Customer Login</h4>
-            {{Former::open_horizontal('clogin','POST',array('class'=>''))}}
-                @if (Session::get('cloginError'))
-                    <div class="alert alert-danger">{{ Session::get('cloginError') }}</div>
-                         <button type="button" class="close" data-dismiss="alert"></button>
-                @endif
-                {{ Former::text('email','Email')->class('span2') }}
-
-                {{ Former::password('password','Password')->class('span2') }}
-
-                {{-- Former::checkbox('remember-me')->label('')->text('Remember Me')->value('remember-me') --}}
-
-                {{ Former::submit('Login')->class('btn btn-primary pull-right') }}
-
-                <div class="" style="display:block;padding-top:20px;text-align:right;clear:both;width:100%;">
-                    <a href="{{ URL::to('account/create') }}" class="" >Create account</a> here
-                </div>
-
-            {{ Former::close() }}
-    </div>
-    <div class="span1"></div>
-    <div class="span4" style="padding-top:15px auto;text-align:right;">
+    <div class="span3" style="padding-left:30px;">
         @include('partials.youtube')
     </div>
 
     {{ HTML::style('css/liquid-slider.css') }}
 
-    <div class="row">
-        <div class="liquid-slider" id="home-slider">
-
-            @foreach($featured as $f)
-             <div>
-                  <h4 class="title">{{ $f['number'].' '.$f['address'].','.$f['city'].' '.$f['state'] }}</h4>
-                  <div class="row">
-                    <div class="span2" >
-                        <img src="{{ $f['defaultpictures']['thumbnail_url']}}" alt="{{ $f['propertyId'] }}" />
-                    </div>
-                    <div class="span10" >
-                        {{ $f['description']}}
-                    </div>
-                  </div>
-             </div>
-            @endforeach
-        </div>
-
-    </div>
 </div>
 
 {{ HTML::script('js/jquery.easing.1.3.js') }}
