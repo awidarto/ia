c2096b44d08792e60b6e2e36560dd45bb99516e9
Andy Awidarto <andy.awidarto@gmail.com>
2014-08-21 10:32:23 +0700

    pdf print

--- public/themes/default/views/pages/faqprint.blade.php
+++ public/themes/default/views/pages/faqprint.blade.php
@@ -21,15 +21,20 @@
             line-height: 20px;
             font-family: Arial,sans-serif;
         }
+
+        .footer{
+            margin-top:25px;
+        }
+
     </style>
-            <div class="row" style="margin:0px;margin-top:20px;padding:0px;">
+            <div class="row-fluid" style="margin:0px;margin-top:20px;padding:0px;">
                 @if(is_null($title))
                     <h1 class="page-header">Untitled</h1>
                 @else
                     <h1 class="page-header">{{ $title }}</h1>
                 @endif
             </div>
-            <div class="row" style="padding:8px;">
+            <div class="row-fluid" style="padding:0px;">
 
                     @if(is_null($faqs))
 
@@ -50,7 +55,15 @@
                                 </ul>
                         {{--@endforeach--}}
                     @endif
-
-                </div>
             </div>
+            <div class="footer">
+                <p class="muted credit">Copyright &copy; 2013 - Investors Alliance USA Property | Terms & Conditions | Privacy Policy</p>
+                <p class="disclaimer" >
+                    <strong>DISCLAIMER:</strong> While every effort is made to ensure that this information is accurate and conforms with all applicable legal requirements it is supplied in good faith as an aid to users. Investors Alliance do not warrant that it is complete, comprehensive or accurate, or commit to its being updated. In no event shall Investors Alliance be liable for any incidental, indirect, consequential or special damages of any kind, or any damages whatsoever, including, without limitation, those resulting from loss of profit, loss of contracts, goodwill, data, information, income, expected savings or business relationships, whether or not advised of the possibility of such damage, arising out of or in connection with the use of this information.
+                </p>
+                <p class="disclaimer">
+                    Copyright - All materials contained herein are, unless otherwise stated, the property of Investors Alliance. Reproduction or retransmission of the materials, in whole or in part, in any manner, without the prior written consent of the copyright holder, is a violation of copyright law.
+                </p>
+            </div>
+
 @stop
