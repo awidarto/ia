71f41acbba8de04e96e670dafb848eea68ade65c
Andy Awidarto <andy.awidarto@gmail.com>
2014-08-26 17:02:49 +0700

    local viceo

--- public/themes/default/views/pages/home.blade.php
+++ public/themes/default/views/pages/home.blade.php
@@ -16,15 +16,16 @@
                       <li>
                         <div style="width:979px;height:373px;min-width:979px;min-height:373px;">
                           @if(isset($slide->videoType) && $slide->videoType == 'youtube')
+                            {{--
                             <video id="home_video" class="video-js vjs-default-skin vjs-big-play-centered"
                               controls preload="auto" width="979" height="373"
                               data-setup='{ "techOrder": ["youtube"], "src": "http://www.youtube.com/watch?v={{$slide->youtubeUrl}}" }'>
                              <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
                             </video>
-
-                            {{--
-                            <object width="970" height="370"><param value="http://www.youtube.com/v/{{$slide->youtubeUrl}}&showsearch=0&rel=0&fs=1&autoplay=0&amp;ap=%2526fmt%3D18" name="movie" /><param value="window" name="wmode" /><param value="true" name="allowFullScreen" /><embed width="970" height="370" wmode="window" allowfullscreen="true" type="application/x-shockwave-flash" src="http://www.youtube.com/v/{{$slide->youtubeUrl}}&showsearch=0&fs=1&rel=0&autoplay=0&amp;ap=%2526fmt%3D18"></embed></object>
                             --}}
+
+                            <object width="970" height="370"><param value="http://www.youtube.com/v/{{$slide->youtubeUrl}}&showsearch=0&rel=0&fs=1&autoplay=0&amp;ap=%2526fmt%3D18" name="movie" /><param value="window" name="wmode" /><param value="true" name="allowFullScreen" /><embed width="970" height="370" wmode="window" allowfullscreen="true" type="application/x-shockwave-flash" src="http://www.youtube.com/v/{{$slide->youtubeUrl}}&showsearch=0&fs=1&rel=0&autoplay=0&amp;ap=%2526fmt%3D18"></embed></object>
+
                           @elseif(isset($slide->videoType) && $slide->videoType == 'local')
                             <video id="home_video" class="video-js vjs-default-skin vjs-big-play-centered"
                               controls preload="auto" width="979" height="373"
@@ -56,15 +57,16 @@
                           <div style="width:45%;height:370px;float:left;margin-right:30px;">
 
                             @if(isset($slide->videoType) && $slide->videoType == 'youtube')
+                              {{--
                               <video id="home_video" class="video-js vjs-default-skin vjs-big-play-centered"
                                 controls preload="auto" width="480" height="373"
                                 data-setup='{ "techOrder": ["youtube"], "src": "http://www.youtube.com/watch?v={{$slide->youtubeUrl}}" }'>
                                <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
                               </video>
-                              {{--
+                              --}}
 
                               <object width="480" height="370"><param value="http://www.youtube.com/v/{{$slide->youtubeUrl}}&showsearch=0&rel=0&fs=1&autoplay=0&amp;ap=%2526fmt%3D18" name="movie" /><param value="window" name="wmode" /><param value="true" name="allowFullScreen" /><embed width="480" height="370" wmode="window" allowfullscreen="true" type="application/x-shockwave-flash" src="http://www.youtube.com/v/{{$slide->youtubeUrl}}&showsearch=0&fs=1&rel=0&autoplay=0&amp;ap=%2526fmt%3D18"></embed></object>
-                              --}}
+
                             @elseif(isset($slide->videoType) && $slide->videoType == 'local')
                               <video id="home_video" class="video-js vjs-default-skin vjs-big-play-centered"
                                 controls preload="auto" width="480" height="373"
