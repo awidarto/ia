64a6e26a992cebb3e4a0c37eda25c54ff56ec9f6
Andy Awidarto <andy.awidarto@gmail.com>
2014-08-26 17:06:11 +0700

    local viceo

--- public/themes/default/views/pages/home.blade.php
+++ public/themes/default/views/pages/home.blade.php
@@ -34,11 +34,7 @@
                              <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
                             </video>
                           @else(isset($slide->videoType) == false)
-                            <video id="home_video" class="video-js vjs-default-skin vjs-big-play-centered "
-                              controls preload="auto" width="979" height="373"
-                              data-setup='{ "techOrder": ["youtube"], "src": "http://www.youtube.com/watch?v={{$slide->youtubeUrl}}" }'>
-                             <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
-                            </video>
+                            <object width="970" height="370"><param value="http://www.youtube.com/v/{{$slide->youtubeUrl}}&showsearch=0&rel=0&fs=1&autoplay=0&amp;ap=%2526fmt%3D18" name="movie" /><param value="window" name="wmode" /><param value="true" name="allowFullScreen" /><embed width="970" height="370" wmode="window" allowfullscreen="true" type="application/x-shockwave-flash" src="http://www.youtube.com/v/{{$slide->youtubeUrl}}&showsearch=0&fs=1&rel=0&autoplay=0&amp;ap=%2526fmt%3D18"></embed></object>
                           @endif
                         </div>
                       </li>
@@ -75,10 +71,7 @@
                                <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
                               </video>
                             @else(isset($slide->videoType) == false)
-                              <video id="home_video" class="video-js vjs-default-skin vjs-big-play-centered"
-                                controls preload="auto" width="480" height="373"
-                                data-setup='{ "techOrder": ["youtube"], "src": "http://www.youtube.com/watch?v={{$slide->youtubeUrl}}" }'>
-                               <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+                              <object width="480" height="370"><param value="http://www.youtube.com/v/{{$slide->youtubeUrl}}&showsearch=0&rel=0&fs=1&autoplay=0&amp;ap=%2526fmt%3D18" name="movie" /><param value="window" name="wmode" /><param value="true" name="allowFullScreen" /><embed width="480" height="370" wmode="window" allowfullscreen="true" type="application/x-shockwave-flash" src="http://www.youtube.com/v/{{$slide->youtubeUrl}}&showsearch=0&fs=1&rel=0&autoplay=0&amp;ap=%2526fmt%3D18"></embed></object>
                               </video>
                             @endif
                           </div>
